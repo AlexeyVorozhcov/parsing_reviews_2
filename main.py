1efac0fb4a1a33b56ad3d35c36bd77e4582dcf98
AlexeyVorozhcov <alexey.vorozhcov@gmail.com>
2022-06-29 15:26:18 +0300

    make defs

--- main.py
+++ main.py
@@ -11,44 +11,62 @@ def start_get_data_from_yandex():
     print("==== End Parsing Yandex Maps ====")
     print()
 
-        
+def get_data_from_file(filename:str):
+    """Получает и возвращает словарь из файла. Может вернуть None."""
+    result = None
+    with open(filename) as json_file:
+            data = json.load(json_file)
+            if data and data != "null":
+                result = data["company_reviews"]
+                result.pop(0)
+    return result            
 
-def analyze_yandex_files(date_start="2010-01-01", date_end="2010-01-01"):
-    all_reviews = []
+
+def clear_reviews(shop:str, reviews: list, date_start, date_end):
+    """Очищает список словарей с отзывами, приводит их в нормальный вид, фильтрует по периоду."""    
+    result=[]
+    for review in reviews:
+        try:
+            review_date = parse(review["review_date"]).date()
+            if review_date >= date_start and review_date <= date_end:
+                clear_review = {
+                    "shop": shop,
+                    "date": review_date,
+                    "stars": review["star_count"],
+                    "text": review["review_text"],
+                }
+                result.append(clear_review)
+        except Exception as e:
+            print("Error:", e)
+            print(shop)
+    return result
+
+def get_filtered_and_cleaned_reviews_from_files_yandex(date_start="2010-01-01", date_end="2010-01-01"):
+    """Возвращает отфильрованные, очищенные и отсортированные отзывы из яндекс-файлов"""
+    result = []
     date_start = parse(date_start).date()
     date_end = parse(date_end).date()
     for shop in shops:
-        name_file = shop["name"] + ".json"
-        with open(name_file) as json_file:
-            data = json.load(json_file)
-            if data and data != "null":
-                reviews = data["company_reviews"]
-                reviews.pop(0)
-                for review in reviews:
-                    try:
-                        review_date = parse(review["review_date"]).date()
-                        if review_date >= date_start and review_date <= date_end:                  
-                            cur_review = {
-                                "shop" : shop["name"],
-                                "date" : review_date,
-                                "stars": review["star_count"],
-                                "text" : review["review_text"],
-                            }
-                            all_reviews.append(cur_review)                            
-                    except Exception as e:
-                        print ("Error:", e)
-                        print(shop["name"])
-                        
-    sorted_reviews = sorted(all_reviews, key=lambda x: x['date'])
-    for review in sorted_reviews:
-        print ("------")
-        print(review['date'])
-        print(review['shop'])
-        print("Оценка:", review['stars'])
-        print(review['text'])
-    return sorted_reviews
+        reviews_of_shop = get_data_from_file(shop["name"] + ".json")
+        if reviews_of_shop: 
+            result.extend(clear_reviews(shop["name"], reviews_of_shop, date_start, date_end))
+    return sorted(result, key=lambda x: x['date'])
+
+def print_review (review:dict):
+    print ("------")
+    print(review['date'])
+    print(review['shop'])
+    print("Оценка:", review['stars'])
+    print(review['text'])
+
+def print_reviews (reviews: list):
+    for review in reviews:
+        print_review(review)
+       
+    
 
 # start_get_data_from_yandex()
 date_start="2022-06-01"
 date_end="2022-07-01"
-analyze_yandex_files(date_start, date_end)
+reviews = get_filtered_and_cleaned_reviews_from_files_yandex(date_start, date_end)
+print_reviews(reviews)
